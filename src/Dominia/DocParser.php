dc221a68c5e8c36194e9c2a8b7cd1fb10292825f
pangodream <development@pangodream.com>
2018-11-02 13:00:26 +0100

    Date validation improvement (regular expression)

--- src/Dominia/DocParser.php
+++ src/Dominia/DocParser.php
@@ -55,7 +55,9 @@ class DocParser
      * @return bool
      */
     private function isValidDate($date){
-        return checkdate((int)substr($date, 3, 2),
+        return preg_match('#[0-3][0-9]\/[0-1][0-9]\/[2][0][0-9][0-9]#', $date)
+               &&
+               checkdate((int)substr($date, 3, 2),
                          (int)substr($date, 0, 2),
                          (int)substr($date, 6, 4));
     }
